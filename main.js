19626c132c4c1346f2e066fa82f9c3870355615c
PunchyCoder <jissensmasher@gmail.com>
2019-05-29 19:46:57 -0700

    Update a bunch
    Should have updated sooner; got caught up in the work.
    Buncha' CSS upgrades
    Implemented a somewhat cohesive color palette
    Buncha' altered JS
    Few HTML alterations

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-  "use strict";
+   "use strict";
 
 
 const url = "https://kitsu.io/api/edge/anime"; //data.attributes.posterImage
@@ -23,34 +23,34 @@ function formatSearchResults(responseJson) {
 	$('#results').empty();
 
 	responseJson['data'].forEach( element => {
-		$('#results').append( // div.card => added .expand (debugging purposes)
-			`<div class="card">
-				 
+		$('#results').append( 
+
+			`<div class="card border">
 				<img src="${element.attributes.posterImage.tiny}">
 				<div class="info">
 					<h3 class="title">${element.attributes['titles']['en']}</h3>
+		
 					<p>Rated <strong>${element.attributes.ageRating}</strong></p>
 					<p>${element.attributes.episodeCount} episodes</p>
-					<p><i class="fas fa-heart"></i> ${element.attributes.averageRating}</p>
+					<p><i class="fas fa-heart" style="color: #ff3575"></i>  ${element.attributes.averageRating}</p>
 
 					<div class="button-container">
-						<input class="synopsis square-btn" type="button" name="synopsis-btn" value="synopsis">
+						<input class="synopsis btn" type="button" name="synopsis-btn" value="synopsis">
 
-						<input class="trailer square-btn" type="button" name="trailer-btn" value="trailer">
+						<input class="trailer btn" type="button" name="trailer-btn" value="trailer">
 					</div>
 				</div>
 				
 				<div class="trailer hidden">
-					<iframe type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${element.attributes.youtubeVideoId}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
+					<h4 class="trailer">Trailer</h4>
+					<iframe class="video" type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${element.attributes.youtubeVideoId}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
 					</iframe>
 				</div>
 
 				<div class="synopsis hidden">
 					<h4 class="synopsis">Synopsis</h4>
-					<p>${element.attributes.synopsis}</p>
+					<p>${element.attributes.synopsis}<input type="button" value="x" onclick="clickSynopsisButton()"></p>
 				</div>
-
-				<div class="video"></div>
 			</div>`);
 
 			addCardButtons();
@@ -150,10 +150,34 @@ function addCardButtons() {
 	});
 }
 
+function clickSynopsisButton(e) {
+	// e.stopImmediatePropagation();
+	const cardEl = $(e.currentTarget).parent().parent().parent().first();
+	const trailerEl = $(e.currentTarget).parent().parent('div.info').next();
+	if (!trailerEl.hasClass('hidden')) {
+		trailerEl.addClass('hidden');
+		cardEl.toggleClass('expand');
+	};
+	$(e.currentTarget).parent().parent().parent().first().toggleClass('expand');
+	$(e.currentTarget).parent().parent('div.info').first().next().next().toggleClass('hidden');
+}
+
+function clickTrailerButton(e) {
+	e.stopImmediatePropagation();
+	const cardEl = $(e.currentTarget).parent().parent().parent().first();
+	const synopsisEl = $(e.currentTarget).parent().parent('div.info').first().next().next();
+	if (!synopsisEl.hasClass('hidden')) {
+		synopsisEl.toggleClass('hidden');
+		cardEl.toggleClass('expand');
+	};
+	$(e.currentTarget).parent().parent().parent().first().toggleClass('expand');
+	$(e.currentTarget).parent().parent('div.info').next().toggleClass('hidden'); // trailer video el needs to be hidden
+}
+
 
 function watchForm() {
 	$('body').toggleClass('fade');
-	searchAnime("death"); // for debugging purpposes...
+	searchAnime("dragon ball"); // for debugging purpposes...
 	$('form').submit(function(e) {
 		e.preventDefault();
 		const searchTitle = $('.search-title').val();
