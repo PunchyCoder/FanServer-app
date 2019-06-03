7adefde3b582fa9888ec2b189819619a72cab699
PunchyCoder <jissensmasher@gmail.com>
2019-06-02 20:07:54 -0700

    Add media query for tablet view

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
    "use strict";
 
+// <iframe class="video" type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${element.attributes.youtubeVideoId}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
+// 					</iframe>
 
 const url = "https://kitsu.io/api/edge/anime"; //data.attributes.posterImage
-const APIKey = '';
+const APIKey = 'AIzaSyBNbDFY9N6VjBIq_3QUODfX0olLhwrEGqg';
 
 function formatQueryParams(params) {
 	const queryItems = Object.keys(params)
@@ -13,49 +15,75 @@ function formatQueryParams(params) {
 
 function displayResults(responseJson, id) {
   console.log("displaying Results...");
-  $(".video").append(
+  $("div.trailer").append(
   	`<iframe type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${id}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
 		</iframe>`
   	)
 };
 
-function formatSearchResults(responseJson) {
-	$('#results').empty();
+function injectTrailers(element) { // what does "response" refer to?
 
-	responseJson['data'].forEach( element => {
-		$('#results').append( 
+	console.log("injectTrailers executing...");
+	// response.data.attributes.titles['en'].forEach( element => {
+	// 		$('#results').find("div.trailer").append(  // .something =>traverses down tree, selects all matching elements	
+	// 	);
 
-			`<div class="card border">
+	const trailerId = searchYoutube(element.attributes['titles']['en']); // returns videoId == trailerId
+
+  	const trailerHTML = `<iframe type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${trailerId}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
+		</iframe>`
+	return trailerHTML;
+}
+
+function injectTemplate(element) {
+	const cardTemplate = 
+		`<div class="card border">
 				<img src="${element.attributes.posterImage.tiny}">
 				<div class="info">
 					<h3 class="title">${element.attributes['titles']['en']}</h3>
-		
+
+					<p><i class="fas fa-heart" style="color: #ff3575"></i>  ${element.attributes.averageRating}% Approval</p>
 					<p>Rated <strong>${element.attributes.ageRating}</strong></p>
 					<p>${element.attributes.episodeCount} episodes</p>
-					<p><i class="fas fa-heart" style="color: #ff3575"></i>  ${element.attributes.averageRating}</p>
 
 					<div class="button-container">
 						<input class="synopsis btn" type="button" name="synopsis-btn" value="synopsis">
-
 						<input class="trailer btn" type="button" name="trailer-btn" value="trailer">
 					</div>
 				</div>
 				
 				<div class="trailer hidden">
 					<h4 class="trailer">Trailer</h4>
-					<iframe class="video" type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${element.attributes.youtubeVideoId}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
-					</iframe>
 				</div>
 
 				<div class="synopsis hidden">
 					<h4 class="synopsis">Synopsis</h4>
 					<p>${element.attributes.synopsis}<input type="button" value="x" onclick="clickSynopsisButton()"></p>
 				</div>
-			</div>`);
+			</div>`;
+			return cardTemplate;
+}
 
-			addCardButtons();
+//===========================
+// function formatSearchResults(responseJson) {
+// 	for( let i = 0; i < 0; i++ ) {
+
+// 	}
+// }
+// ==========================
+
+function formatSearchResults(responseJson) {
+	$('#results').empty();
+
+	responseJson['data'].forEach( element => {
+		$('#results').append( 
+			injectTemplate(element)
+		);
+		// $('div.trailer').append(
+		// 	injectTrailers(element) // hopefully just grabs trailer for each individual element...
+		// );
+		addCardButtons();
 	})
-			
 }
 
 // Search Youtube API--
@@ -68,116 +96,74 @@ function searchYoutube(search) {
 		type: 'video',
 		videoEmbeddable: true
 	};
-
 	const queryString = formatQueryParams(params);
 	const url = 'https://www.googleapis.com/youtube/v3/search';
 	const searchURL = url + '?' + queryString;
-  	
+
+  	console.log(searchURL) // Console.log
+
   	fetch(searchURL)
   	.then(response => response.json())
   	.then(responseJson => {
-  		console.log(responseJson)
-  		const videoID = responseJson['data']['youtubeVideoId']; // need to display video forEach result > not just item[0]
-  		displayResults(responseJson, videoID);
+  		console.log("youtube Response " + responseJson)
+  		const videoID = responseJson.items[0].id.videoId; // grabs first result of each search.
+  		return videoID;
+  		//displayResults(responseJson, videoID);
   	})
 }
 
 
 function searchAnime(search) { //  Use anime title as search!
 	const params = {
-		'filter[text]': search
+		'filter[text]': search,
+		'page[limit]': 10,
+		'page[offset]': 0
 	}
-
 	const queryString = formatQueryParams(params);
 	const searchURL = url + '?' + queryString;
-	console.log(searchURL);
-
+	console.log(searchURL); // Console.log
 	fetch(searchURL)
 	.then(response => response.json())
 	.then(responseJson => {
-		console.log(responseJson['data'][0].attributes.posterImage.medium)
-		console.log(`Fan Rating: ${responseJson['data'][0].attributes.popularityRank}`)
 		formatSearchResults(responseJson);
-		return responseJson		
+		return responseJson;	
 	})
-	// .then( responseJson => {
-	// 	const animeTitle = responseJson['data'][0].attributes.slug;
-	// 	searchYoutube(animeTitle);
-	// });
+	// Run call-search to youtube for video on "trailer button" click OR 
 }
 
 function addCardButtons() {
-	
-	// $('input.synopsis').on("click", function(e) { //need to select only
-	// 	e.stopImmediatePropagation();
-	// 	// $(e.currentTarget).next().toggleClass('hidden');
-	// 	$(document.querySelector('div.'))
-	// });
-
 
 	// Deals with "synopsis" button "logic"
 	$('input.synopsis').on("click", function(e) { //need to select only
 		e.stopImmediatePropagation();
-		
-		const cardEl = $(e.currentTarget).parent().parent().parent().first();
-		const trailerEl = $(e.currentTarget).parent().parent('div.info').next();
-
+		const cardEl = $(e.currentTarget).parents('div.card').first();
+		const trailerEl = $(e.currentTarget).parents('div.info').next();
 		if (!trailerEl.hasClass('hidden')) {
 			trailerEl.addClass('hidden');
 			cardEl.toggleClass('expand');
 		};
-
-		$(e.currentTarget).parent().parent().parent().first().toggleClass('expand');
-
-		$(e.currentTarget).parent().parent('div.info').first().next().next().toggleClass('hidden');
+		cardEl.toggleClass('expand');
+		$(e.currentTarget).parents('div.info').first().nextAll('div.synopsis').toggleClass('hidden');
 	});
 
 	// Deals with "trailer" button "logic"
 	$('input.trailer').on("click", function(e) { //need to select only
 		e.stopImmediatePropagation();
-
-		const cardEl = $(e.currentTarget).parent().parent().parent().first();
-		const synopsisEl = $(e.currentTarget).parent().parent('div.info').first().next().next();
-		
+		const cardEl = $(e.currentTarget).parents('div.card').first();
+		const synopsisEl = $(e.currentTarget).parents('div.info').first().nextAll('div.synopsis');
 		if (!synopsisEl.hasClass('hidden')) {
 			synopsisEl.toggleClass('hidden');
 			cardEl.toggleClass('expand');
 		};
-
-		$(e.currentTarget).parent().parent().parent().first().toggleClass('expand');
-
+		cardEl.toggleClass('expand');
 		$(e.currentTarget).parent().parent('div.info').next().toggleClass('hidden'); // trailer video el needs to be hidden
 	});
 }
 
-function clickSynopsisButton(e) {
-	// e.stopImmediatePropagation();
-	const cardEl = $(e.currentTarget).parent().parent().parent().first();
-	const trailerEl = $(e.currentTarget).parent().parent('div.info').next();
-	if (!trailerEl.hasClass('hidden')) {
-		trailerEl.addClass('hidden');
-		cardEl.toggleClass('expand');
-	};
-	$(e.currentTarget).parent().parent().parent().first().toggleClass('expand');
-	$(e.currentTarget).parent().parent('div.info').first().next().next().toggleClass('hidden');
-}
-
-function clickTrailerButton(e) {
-	e.stopImmediatePropagation();
-	const cardEl = $(e.currentTarget).parent().parent().parent().first();
-	const synopsisEl = $(e.currentTarget).parent().parent('div.info').first().next().next();
-	if (!synopsisEl.hasClass('hidden')) {
-		synopsisEl.toggleClass('hidden');
-		cardEl.toggleClass('expand');
-	};
-	$(e.currentTarget).parent().parent().parent().first().toggleClass('expand');
-	$(e.currentTarget).parent().parent('div.info').next().toggleClass('hidden'); // trailer video el needs to be hidden
-}
-
-
 function watchForm() {
 	$('body').toggleClass('fade');
 	searchAnime("dragon ball"); // for debugging purpposes...
+
 	$('form').submit(function(e) {
 		e.preventDefault();
 		const searchTitle = $('.search-title').val();
