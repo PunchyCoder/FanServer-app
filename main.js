34b442cc99bafe03bf1086f7f8f629625ac7df15
PunchyCoder <jissensmasher@gmail.com>
2019-06-06 14:34:36 -0700

    Improve accessibility, responsiveness, ...
    Lose youtube, stick to Kitsu API..

--- main.js
+++ main.js
@@ -1,110 +1,10 @@
    "use strict";
 
 const url = "https://kitsu.io/api/edge/anime"; //data.attributes.posterImage
-const APIKey = 'AIzaSyBNbDFY9N6VjBIq_3QUODfX0olLhwrEGqg';
-
-function formatQueryParams(params) {
-	const queryItems = Object.keys(params)
-	.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-	return queryItems.join('&');
-}
-
-// function displayResults(responseJson, id) {
-//   console.log("displaying Results...");
-//   $("div.trailer").append(
-//   	`<iframe type="text/html" width="300" height="200" src="https://www.youtube.com/embed/${id}?modestbranding=1&amp;rel=0&amp;showinfo=0" frameborder="0" ><br />
-// 		</iframe>`
-//   	)
-// };
+const APIKey = 'AIzaSyARlWxggTnR6mIMhHdyW2Bnf7BomchaBaE';
 
 
-function injectTemplate(element, trailerId) {
-	const cardTemplate = 
-		`<div class="card border">
-			<img src="${element.attributes.posterImage.tiny}">
-			<div class="info">
-				<h3 class="title">${element.attributes['titles']['en']}</h3>
-
-				<p><i class="fas fa-heart" style="color: #ff3575"></i>  ${element.attributes.averageRating}% Approval</p>
-				<p>Rated <strong>${element.attributes.ageRating}</strong></p>
-				<p>${element.attributes.episodeCount} episodes</p>
-
-				<div class="button-container">
-					<input class="synopsis btn" type="button" name="synopsis-btn" value="synopsis">
-					<input class="trailer btn" type="button" name="trailer-btn" value="trailer">
-				</div>
-			</div>
-			
-			<div class="trailer hidden">
-				<h4 class="trailer">Trailer</h4>
-				<iframe width="420" height="315" src=\`https://www.youtube.com/embed/${trailerId}\`></iframe>
-			</div>
-
-			<div class="synopsis hidden">
-				<h4 class="synopsis">Synopsis</h4>
-				<p>${element.attributes.synopsis}<input type="button" value="x" onclick="clickSynopsisButton()"></p>
-			</div>
-		</div>`;
-	return cardTemplate;
-}
-
-
-function formatSearchResults(responseJson) {
-	$('#results').empty();
-	responseJson['data'].forEach( element => {
-		$('#results').append( 
-			injectTemplate(element, searchYoutube(element.attributes.titles.en))
-		);
-		addCardButtons();
-	})
-}
-
-// ----- Call to YouTube
-function searchYoutube(search) {
-	const params = {
-		part: 'snippet',
-		key: APIKey,
-		q: search + ' anime trailer',
-		type: 'video',
-		maxResults: 5,
-		videoEmbeddable: true
-	};
-	const queryString = formatQueryParams(params);
-	const url = 'https://www.googleapis.com/youtube/v3/search';
-	const searchURL = url + '?' + queryString;
-
-  	console.log("Youtube searchURL: " + searchURL) // Console.log
-
-  	fetch(searchURL)
-  	.then(response => response.json())
-  	.then(responseJson => {
-  		console.log("youtube Response " + responseJson)
-  		const trailerId = responseJson.items[0].id.videoId; // grabs first result of each search.
-  		console.log('trailerID = ' + trailerId )
-  		return trailerId;
-  	})
-}
-
-// ----- Call to Kitsu API
-function searchAnime(search) { //  Use anime title as search!
-	const params = {
-		'filter[text]': search,
-		'page[limit]': 10,
-		'page[offset]': 0
-	}
-	const queryString = formatQueryParams(params);
-	const searchURL = url + '?' + queryString;
-
-	console.log(searchURL); // Console.log
-
-	fetch(searchURL)
-	.then(response => response.json())
-	.then(responseJson => {
-		formatSearchResults(responseJson);
-	})
-}
-
-function handle_Synopsis() {
+function handle_SynopsisButton() {
 	$('input.synopsis').on("click", function(e) { //need to select only
 		e.stopImmediatePropagation();
 		const cardEl = $(e.currentTarget).parents('div.card').first();
@@ -118,7 +18,7 @@ function handle_Synopsis() {
 	});
 }
 
-function handle_Trailer() {
+function handle_TrailerButton() {
 	$('input.trailer').on("click", function(e) { //need to select only
 		e.stopImmediatePropagation();
 		const cardEl = $(e.currentTarget).parents('div.card').first();
@@ -133,14 +33,80 @@ function handle_Trailer() {
 }
 
 function addCardButtons() {
-	handle_Synopsis();
-	handle_Trailer();
+	handle_SynopsisButton();
+	handle_TrailerButton();
+}
+
+function injectTemplate(element, count) {
+	const cardTemplate = 
+		`<div id=${count} class="card border">
+			<span>${count}</span>
+			<img src="${element.attributes.posterImage.tiny}">
+			<div class="info">
+				<h3 class="title">${element.attributes['titles']['en']}</h3>
+
+				<p><i class="fas fa-heart" style="color: #ff3575"></i>  ${element.attributes.averageRating}% Approval</p>
+				<p>Rated <strong>${element.attributes.ageRating}</strong></p>
+				<p>${element.attributes.episodeCount} episodes</p>
+
+				<div class="button-container">
+					<input class="synopsis btn" type="button" name="synopsis-btn" value="Synopsis">
+					<input class="trailer btn" type="button" name="trailer-btn" value="Trailer">
+				</div>
+			</div>
+			
+			<div class="trailer hidden">
+				<h4 class="trailer">Trailer</h4>
+				<iframe width="320" height="240" src="https://www.youtube.com/embed/${element.attributes.youtubeVideoId}"></iframe>
+			</div>
+
+			<div class="synopsis hidden">
+				<h4 class="synopsis">Synopsis</h4>
+				<p>${element.attributes.synopsis}</p>
+			</div>
+		</div>`;
+	return cardTemplate;
+}
+
+function formatSearchResults(responseJson) {
+	$('#results').empty();
+	$('#results').append( `<h2>Results</h2>` );
+	let count = 0;
+	responseJson['data'].forEach( element => {
+		count++;
+		$('#results').append( 
+			injectTemplate(element, count)
+		);
+		addCardButtons();
+	})
+}
+
+function formatQueryParams(params) {
+	const queryItems = Object.keys(params)
+	.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+	return queryItems.join('&');
+}
+
+function searchAnime(search) {
+	const params = {
+		'filter[text]': search,
+		'page[limit]': 10,
+		'page[offset]': 0 
+	}
+	const queryString = formatQueryParams(params);
+	const searchURL = url + '?' + queryString;
+
+	fetch(searchURL)
+	.then(response => response.json())
+	.then(responseJson => {
+		formatSearchResults(responseJson);
+	})
 }
 
 function watchForm() {
+
 	$('body').toggleClass('fade');
-	searchAnime("dragon ball"); // for debugging purpposes...
-	searchYoutube('dragon ball');
+	//searchAnime("dragon ball"); // for debugging purpposes...
 
 	$('form').submit(function(e) {
 		e.preventDefault();
